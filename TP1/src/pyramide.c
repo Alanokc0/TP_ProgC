0022a6783c1843f610b4af737cfc52b5a327cad2
Alanokc0 <99540574+Alanokc0@users.noreply.github.com>
2025-11-21 15:48:08 +0100

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 5;   // hauteur de la pyramide
+    int i, j;
+
+    printf("Generation d'une pyramide de hauteur %d :\n\n", n);
+
+    for (i = 1; i <= n; i++)
+    {
+        // ---- Affichage des espaces pour centrer ----
+        for (j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        // ---- Nombres croissants ----
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        // ---- Nombres décroissants ----
+        for (j = i - 1; j >= 1; j--)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("\nPyramide complete.\n");
+
+    return 0;
+}
 
