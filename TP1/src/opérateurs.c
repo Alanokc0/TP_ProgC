834471936b689947de6b2fe09a93b6b381ecf7d0
Alanokc0 <99540574+Alanokc0@users.noreply.github.com>
2025-11-21 15:24:29 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+ int a = 16;
+ int b = 3;
+
+ int add = a+b;
+ int sous = a-b;
+ int mul = a*b;
+ int div = a/b;
+
+ printf("%d\n",a+b,add);
+ printf("%d\n",a-b,sous);
+ printf("%d\n",a*b,mul);
+ printf("%d\n",a/b,div);
+
+
+ return 0; 
+}
